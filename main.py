6d0df788aaa2b083e448d97a7d0802c70c24c30a
AmirHusnutdinov <amir800900@mail.ru>
2023-01-15 14:52:53 +0500

    Amir Khusnutdinov start background V3 AND Rules V1

--- main.py
+++ main.py
@@ -12,43 +12,43 @@ screen = pygame.display.set_mode(size)
 manager = pygame_gui.UIManager((1100, 600))
 
 mass = []
-backgrounds = (os.listdir(f'{os.path.abspath("data")}'))
+backgrounds = (os.listdir(f'{os.path.abspath("BackGrounds")}'))
 for i in backgrounds:
-    mass.append(pygame.image.load(f'data/{i}'))
+    mass.append(pygame.image.load(f'BackGrounds/{i}'))
 image = mass[random.randrange(0, len(mass))]
-l1 = pygame_gui.elements.UILabel(relative_rect=pygame.Rect(450, 100, 200, 100),
-                                 text='Mega Game',
 
-                                 manager=manager)
-l1.set_text_scale(scale=2)
 choose_level = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(
                                     options_list=['Industrial Zone', 'Castle Zone', 'Green Zone'],
                                     starting_option='Industrial Zone',
                                     relative_rect=pygame.Rect(300, 275, 150, 50),
                                     manager=manager,)
 
-l2 = pygame_gui.elements.UILabel(relative_rect=pygame.Rect(100, 200, 200, 100),
-                                 text='Choose level',
-                                 manager=manager)
-
-
-btn2 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((500, 275), (150, 60)),
+start_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((500, 275), (150, 60)),
                                     text='Start',
                                     tool_tip_text='Нажми ты уже эту кнопку!!!',
                                     manager=manager)
-btn3 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((700, 275), (150, 50)),
-                                    text='Правила',
+
+rule_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((700, 275), (150, 50)),
+                                    text='Rules',
+                                    tool_tip_text='Нажми ты уже эту кнопку!!!',
+                                    manager=manager)
+
+back_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((950, 0), (150, 50)),
+                                    text='Come back',
                                     tool_tip_text='Нажми ты уже эту кнопку!!!',
                                     manager=manager)
 
 clock = pygame.time.Clock()
 running = True
-count = 0
+position = 0
 v = 40
+mode = 'main'
 
 while running:
     time_delta = clock.tick(60) / 1000
+    
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             confirmation_dialog = pygame_gui.windows.UIConfirmationDialog(
                 rect=pygame.Rect((400, 100), (300, 200)),
@@ -56,30 +56,67 @@ while running:
                 window_title='Подтверждение',
                 action_long_desc='Вы уверены, что хотите выйти ?',
                 action_short_name='YES',
-                blocking=True
-            )
+                blocking=True)
+
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
                 running = False
         manager.process_events(event)
     manager.update(time_delta)
-    screen.blit(image, (count, 0))
-    pygame.draw.line(screen, 'white', (250, 250), (300, 300), 4)
-    pygame.draw.line(screen, 'white', (300, 300), (300, 280), 3)
-    pygame.draw.line(screen, 'white', (300, 300), (280, 300), 3)
-    if count > - 100:
-        count -= v * clock.tick(60) / 1000
+    
+    if start_btn.check_pressed():
+        pass
+
+    elif rule_btn.check_pressed():
+        mode = 'rules'
+
+    screen.blit(image, (position, 0))
+    if position > - 100:
+        position -= v * clock.tick(60) / 1000
     else:
         last_im = image
         image = mass[random.randrange(0, len(mass))]
         while image == last_im:
             image = mass[random.randrange(0, len(mass))]
-        count = random.randrange(-5, 2)
-    if btn2.check_pressed():
-        print(1)
-    elif btn3.check_pressed():
-        print(2)
+        position = random.randrange(-5, 2)
+
+    if mode == 'main':
+        back_btn.hide()
+        im2 = pygame.image.load('img/Name.png')
+        logo = im2.get_rect(
+            bottomright=(700, 250))
+        screen.blit(im2, logo)
+
+        im3 = pygame.image.load('img/Choose_level.png')
+        logo2 = im3.get_rect(
+            bottomright=(300, 440))
+        screen.blit(im3, logo2)
+
+        graffiti = (os.listdir(f'{os.path.abspath("graffiti")}'))
+        coords = [(300, 500), (700, 500), (150, 400),
+                  (500, 450), (900, 100), (300, 150),
+                  (960, 300), (600, 100), (980, 500)]
+        for i in range(len(graffiti)):
+            pict = pygame.image.load(f'graffiti/{graffiti[i]}')
+            graf = pict.get_rect(bottomright=(coords[i]))
+            screen.blit(pict, graf)
+
+    elif mode == 'rules':
+        back_btn.show()
+        start_btn.hide()
+        rule_btn.hide()
+        choose_level.hide()
+        if back_btn.check_pressed():
+            mode = 'main'
+            start_btn.show()
+            rule_btn.show()
+            choose_level.show()
+
+        pict2 = pygame.image.load('rules.png')
+        graf2 = pict2.get_rect(bottomright=(1100, 600))
+
+        screen.blit(pict2, graf2)
+
     manager.draw_ui(screen)
-    l1.set_text_scale(scale=3)
     pygame.display.flip()
 quit()
